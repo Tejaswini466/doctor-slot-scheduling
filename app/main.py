d43ff35336c36feca30a854627ed94c707d15b90
Tejaswini466 <nagatejaswini.p@gmail.com>
2026-07-26 15:55:38 +0530

    postgreSQL and SQLalchemy connection

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.db.database import SessionLocal
 
 app = FastAPI(
     title="Doctor Slot Scheduling API",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 @app.get("/")
 def home():
     return {"message": "Doctor Slot Scheduling API is running!"}
+
+@app.get("/db-test")
+def db_test():
+    db = SessionLocal()
+    try:
+        db.execute(text("SELECT 1"))
+        return {"message": "Database connection successful!"}
+    finally:
+        db.close()
